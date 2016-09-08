0f672ae4dd4098fd7c9724c963db0ef61827a745
OwenLaRosa <owen.larosa@outlook.com>
2016-09-07 22:53:03 -0400

    feat: Get place holder data for the chart

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailActivityFragment.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailActivityFragment.java
@@ -8,11 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.db.chart.model.LineSet;
 import com.db.chart.view.LineChartView;
 import com.sam_chordas.android.stockhawk.R;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -53,9 +58,29 @@ public class StockDetailActivityFragment extends Fragment {
     // this should be moved outside the fragment
     private void getGraphData() throws IOException {
         // create a request, use hardcoded symbol to fetch data for now
+        // URL referenced from https://discussions.udacity.com/t/plotting-the-stock-price-over-time-within-the-stock-hawk-project/159569/8
         Request request = new Request.Builder().url("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22YHOO%22%20and%20startDate%20%3D%20%222009-09-11%22%20and%20endDate%20%3D%20%222010-03-10%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").build();
         Response response = mClient.newCall(request).execute();
-        Log.d(LOG_TAG, response.body().string());
+        String result = response.body().string();
+        try {
+            showDataInGraph(result);
+        } catch (JSONException e) {}
+    }
+
+    private void showDataInGraph(String json) throws JSONException {
+        JSONObject rootObject = new JSONObject(json);
+        JSONObject queryObject = rootObject.getJSONObject("query");
+        JSONObject resultsObject = queryObject.getJSONObject("results");
+        JSONArray quotes = resultsObject.getJSONArray("quote");
+        LineSet dataSet = new LineSet();
+        for (int i = 0; i < quotes.length(); i++) {
+            JSONObject quote = quotes.getJSONObject(i);
+            float close = (float) quote.getDouble("Close");
+            // no label for now
+            dataSet.addPoint("", close);
+        }
+        mLineGraph.addData(dataSet);
+        mLineGraph.show();
     }
 
 }
